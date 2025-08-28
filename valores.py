d9db5c9073d37900745b3f23823e926d846e5661
MeirejaneChaves605 <meirejane317@gmail.com>
2025-08-28 19:18:22 -0300

    Delete valores.py

--- valores.py
+++
@@ -1,9 +0,0 @@
-# lê os valores de A e B
-A = int(input("Digite o valor de A: "))
-B = int(input("Digite o valor de B: "))
-# faz a troca dos valores
-temp = A
-A = B
-B = temp
-# imprime os valores trocados
-print(f"Depois da troca: A = {A}, B = {B}")
